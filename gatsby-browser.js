a28353a0e692aceeca501f8a2216552e26aafa3d
WonhyeongLee <bigfile57@naver.com>
2023-06-07 23:06:19 +0900

    Fix #1: normalize.css emotion으로 적용하는 방식으로 수정
    추가로 초기화하고 싶은 부분은 extraNormalize.js에 추가해서 사용할 수 있게 했습니다.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,25 +1,36 @@
 import React from "react"
+// normalize with emotion
+import { Global, css } from "@emotion/react"
+import * as normalize from "normalize.css"
+import extraNormalize from "./src/styles/extraNormalize"
 // custom typefaces
 import "@fontsource/montserrat/variable.css"
 import "@fontsource/merriweather"
-// normalize CSS across browsers
-import "./src/normalize.css"
 // custom CSS styles
 import "./src/style.css"
-
 // Highlighting for code blocks
 import "prismjs/themes/prism.css"
 // import Layout from "./src/components/layout"
 import TagProvider from "./src/context/TagProvider"
+//SSG에서 Font Awesome 스타일이 head에 추가되는 시점을 조정
+import "@fortawesome/fontawesome-svg-core/styles.css"
+import { config } from "@fortawesome/fontawesome-svg-core"
+config.autoAddCss = false
 
 export const wrapRootElement = ({ element }) => {
-  return <TagProvider>{element}</TagProvider>
+  return (
+    <>
+      <Global
+        styles={css`
+          ${normalize}
+          ${extraNormalize}
+        `}
+      />
+      <TagProvider>{element}</TagProvider>
+    </>
+  )
 }
 
 // export const wrapPageElement = ({ element, props }) => {
 //   return <Layout {...props}>{element}</Layout>
 // }
-//SSG에서 Font Awesome 스타일이 head에 추가되는 시점을 조정
-import "@fortawesome/fontawesome-svg-core/styles.css"
-import { config } from "@fortawesome/fontawesome-svg-core"
-config.autoAddCss = false
---
+++ src/styles/extraNormalize.js
@@ -0,0 +1,9 @@
+const extraNormalize = `
+  ol, ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+`
+
+export default extraNormalize
