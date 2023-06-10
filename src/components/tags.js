f3c7632d19447ac0272477e33a404508c812486b
WonhyeongLee <bigfile57@naver.com>
2023-06-10 13:02:07 +0900

    style #3:필터링중인 태그 색상추가
    hover 했을 때 효과를 필터링 중인 태그에 적용되도록 Tag 컴포넌트 스타일 수정했습니다.
    태그가 선택된 경우에 'tagStyle'과 'selectedTagStyle'을 적용하도록 했습니다.
    hover와 선택된 경우의 스타일이 같기 때문에 hoverAndSelectedTagStyle 변수를 작성하고 재사용했습니다.

--- src/components/tags.js
+++ src/components/tags.js
@@ -1,24 +1,14 @@
 import React, { useContext } from "react"
 import { Link } from "gatsby"
 import kebabCase from "lodash/kebabCase"
-import { css, cx } from "@emotion/css"
+import { css } from "@emotion/react"
 import TagContext from "../context/TagContext"
 
-// styled 방식
-// import styled from "@emotion/styled"
-// const StyledTag = styled(Link)`
-//   display: inline-block;
-//   margin: 0 5px;
-//   padding: 5px 10px;
-//   background-color: #f8f8f8;
-//   border-radius: 20px;
-//   font-size: 14px;
-//   text-decoration: none;
-//   color: #333;
-//   &:hover {
-//     background-color: #e0e0e0;
-//   }
-// `
+const hoverAndSelectedTagStyle = css`
+  font-weight: var(--fontWeight-bold);
+  color: #f31010;
+  background-color: #e0e0e0;
+`
 
 const tagStyle = css`
   display: inline-block;
@@ -29,19 +19,20 @@ const tagStyle = css`
   text-decoration: none;
   color: #333;
   &:hover {
-    font-weight: var(--fontWeight-bold);
-    color: #f31010;
-    background-color: #e0e0e0;
+    ${hoverAndSelectedTagStyle}
   }
+`
+const selectedTagStyle = css`
+  ${hoverAndSelectedTagStyle}
 `
 
 const Tag = ({ tag }) => {
-  const { setSelectedTag } = useContext(TagContext)
+  const { selectedTag, setSelectedTag } = useContext(TagContext)
 
   return (
     <Link
       to={`/tags/${kebabCase(tag)}`}
-      className={cx(tagStyle)}
+      css={tag === selectedTag ? [tagStyle, selectedTagStyle] : tagStyle}
       onClick={event => {
         event.preventDefault()
         setSelectedTag(tag)
